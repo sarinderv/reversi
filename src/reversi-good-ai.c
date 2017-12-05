9f799800cfa04b2e9cba661545e7dd11fa3b3aa2
Sarinder Virk <savirk@pc21.cs.ucdavis.edu>
2017-12-04 22:07:19 -0800

    fixed a major bug

--- src/reversi-good-ai.c
+++ src/reversi-good-ai.c
@@ -12,11 +12,12 @@
 
 #include "reversi.h"
 
-#define DEPTH 1
+#define DEPTH 4
 
 typedef enum { false, true } bool;
 
-int tryMove(ull moves, int moveNum, int color, Board *b) {
+Move toMove(ull moves, int moveNum)
+{
     // clz returns the bit number of the first bit set, counting from the MSB.
     int highestBit = __builtin_clzll(moves);
 
@@ -29,6 +30,12 @@ int tryMove(ull moves, int moveNum, int color, Board *b) {
     }
 
     Move m = BIT_TO_MOVE(highestBit);
+    return m;
+}
+
+int tryMove(ull moves, int moveNum, int color, Board *b)
+{
+    Move m = toMove(moves, moveNum);
 
     // Set the third parameter to 1, to echo disks flipped.
     int nflips = FlipDisks(m, b, color, 0, 1);
@@ -45,6 +52,7 @@ int minimax(Board *b, int depth, int startColor, bool maximizingPlayer)
     // alternate color depending on the depth
     int color = maximizingPlayer ? startColor : OTHERCOLOR(startColor);
     char c_c = color ==  X_BLACK ? 'X' : 'O';
+
     Board legal_moves;
     int num_moves = EnumerateLegalMoves(*b, color, &legal_moves);
     if (depth <= 0 || num_moves <= 0) {
@@ -54,50 +62,48 @@ int minimax(Board *b, int depth, int startColor, bool maximizingPlayer)
     printf("at depth %d, %c has %d possible moves\n", depth, c_c, num_moves);
 
     ull moves = legal_moves.disks[color];
+    int moveNum = 0, bestMoveNum = 0, bestValue = INT_MIN;
 
     if (maximizingPlayer) {
-        int bestValue = INT_MIN;
-        for (int moveNum = 0; moveNum < num_moves; moveNum++) {
+        for (; moveNum < num_moves; moveNum++) {
             Board c = *b; // copy
             int nFlips = tryMove(moves, moveNum, color, &c);
-            //printf("maximizingPlayer=%c, depth=%d, move=(%d,%d) ... ", c_c, depth, m.row, m.col);
             if (nFlips != 0) {
                 int v = minimax(&c, depth - 1, startColor, false);
                 if (v > bestValue) {
-                    //PrintBoard(*b);
-                    //PrintBoard(c);
-                    b = &c;
                     bestValue = v;
-                    printf("at depth %d, moveNum=%d for %c yielded the bestValue=%d (max)\n", depth, moveNum, c_c, bestValue);
+                    bestMoveNum = moveNum;
                 }
             }
         }
-        return bestValue;
     }
     else {
-        int bestValue = INT_MAX;
-        for (int moveNum = 0; moveNum < num_moves; moveNum++) {
+        bestValue = INT_MAX;
+        for (; moveNum < num_moves; moveNum++) {
             Board c = *b; // copy
             int nFlips = tryMove(moves, moveNum, color, &c);
-            //printf("minimizingPlayer=%c, depth=%d, move=(%d,%d) ... ", c_c, depth, m.row, m.col);
             if (nFlips != 0) {
-                int v = minimax(&c, depth - 1, startColor, false);
+                int v = minimax(&c, depth - 1, startColor, true);
                 if (v < bestValue) {
-                    //PrintBoard(*b);
-                    //PrintBoard(c);
-                    b = &c;
                     bestValue = v;
-                    printf("at depth %d, moveNum=%d for %c yielded the bestValue=%d (min)\n", depth, moveNum, c_c, bestValue);
+                    bestMoveNum = moveNum;
                 }
             }
         }
-        return bestValue;
     }
+
+    Move move = toMove(moves, bestMoveNum);
+    printf("at depth %d, move(%d,%d) for %c yields %s bestValue=%d\n", depth, move.row, move.col, c_c, maximizingPlayer ? "max" : "min", bestValue);
+
+    if (depth == DEPTH) // reached the end, update the board now
+        tryMove(moves, moveNum, color, b);
+
+    return bestValue;
 }
 
 int GoodAITurnSequential(Board *b, int color)
 {
-    int num_disks = minimax(b, DEPTH, color, true);
+    minimax(b, DEPTH, color, true);
 
     Board legal_moves;
     int num_moves = EnumerateLegalMoves(*b, color, &legal_moves);
